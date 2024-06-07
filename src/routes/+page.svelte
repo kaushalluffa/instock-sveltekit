1bd5bbcf0641aee404b6e6376f03db1e41bcced0
kaushalluffa <kaushalluffa91@gmail.com>
2024-06-06 21:07:04 -0400

    feat(add-warehouses-page-header): add warehouses page header with search and add btn

---
+++ src/routes/+page.svelte
@@ -0,0 +1,56 @@
+<div class="warehouses-page">
+	<div class="warehouses__header">
+		<h1 class="warehouses__heading">Warehouses</h1>
+		<input placeholder="Search..." class="warehouses__search" />
+		<a href="/" class="warehouses__add">+ Add New Warehouse</a>
+	</div>
+</div>
+
+<style lang="scss">
+	.warehouses {
+		&-page {
+			padding: 1rem;
+			background-color: $in-stock-white;
+			margin: 1rem;
+			border-radius: 4px;
+			height: 100%;
+			margin-top: -4rem;
+			border-bottom: 1px solid $in-stock-cloud;
+		}
+		&__header {
+			display: flex;
+			flex-direction: column;
+			gap: 1rem;
+			padding: 1rem;
+			@include tablet {
+				flex-direction: row;
+				justify-content: space-between;
+			}
+		}
+		&__heading {
+			font-size: $h1M;
+			font-weight: normal;
+			@include laptop {
+				flex: 2;
+			}
+		}
+		&__search {
+			border-radius: 20px;
+			border: 1px solid $in-stock-cloud;
+			min-height: 36px;
+			padding: 0 1rem;
+			flex: 1;
+		}
+		&__add {
+			background-color: $in-stock-indigo;
+			color: $in-stock-white;
+			border-radius: 20px;
+			padding: 0.5rem 1rem;
+			width: fit-content;
+			text-decoration: none;
+			text-align: center;
+			flex: 1;
+			width: 100%;
+		}
+	}
+</style>
