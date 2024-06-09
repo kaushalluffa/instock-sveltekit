b5048e140111a1c2134c79bc3ad850e7685c7e8b
kaushalluffa <kaushalluffa91@gmail.com>
2024-06-09 15:45:05 -0400

    feat(add-form-page): add form page component
    added form page component and removed duplicated code in add and edit warehouse route

---
+++ src/lib/components/FormPageHeader.svelte
@@ -0,0 +1,39 @@
+<script>
+	import backArrow from '$assets/backArrow.svg';
+	export let heading;
+	export let backBtnLink = '/';
+</script>
+
+<div class="form-page-header">
+	<div class="form-page-header--header">
+		<a href="{backBtnLink}">
+			<img class="form-page-header--backBtn" src={backArrow} alt="back arrow" />
+		</a>
+		<h1 class="form-page-header--heading">{heading}</h1>
+	</div>
+</div>
+
+<style lang="scss">
+	.form-page-header {
+		border-bottom: 1px solid $in-stock-cloud;
+		padding: 1rem;
+		&--header {
+			width: 100%;
+			display: flex;
+			align-items: center;
+			gap: 1rem;
+		}
+		&--heading {
+			font-size: $h1M;
+			line-height: 36px;
+			font-weight: 600;
+			@include laptop {
+				flex: 2;
+			}
+		}
+		&--backBtn {
+			width: 24px;
+			height: 24px;
+		}
+	}
+</style>
