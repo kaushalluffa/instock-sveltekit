8b0081aedfb0556f093da110209b7f36bc614518
kaushalluffa <kaushalluffa91@gmail.com>
2024-06-09 22:42:27 -0400

    feat(add-inventory-page): add create inventory page

---
+++ src/routes/add/inventory/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+	import { FormPageHeader, InventoryForm } from '$components';
+</script>
+
+<FormPageHeader heading="Add New Inventory Item" backBtnLink="/inventory" />
+<InventoryForm submitBtnText="+ Add Item" />
