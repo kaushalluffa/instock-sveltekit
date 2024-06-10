f6fb44f8d95a78c9655556628aa7742a8b8e9881
kaushalluffa <kaushalluffa91@gmail.com>
2024-06-09 22:41:56 -0400

    feat(update-main-page): update home page and removed duplicated code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,20 @@
 
 <Header />
 <div class="content">
-	<slot />
+	<div class="page-content-wrapper">
+		<slot />
+	</div>
 </div>
 <Footer />
+
+<style lang="scss">
+	.page-content-wrapper {
+		background-color: $in-stock-white;
+		border-radius: 4px;
+		height: 100%;
+		border-bottom: 1px solid $in-stock-cloud;
+		max-width: 1020px;
+		width: 100%;
+		margin: -4rem 1rem 1rem 1rem;
+	}
+</style>
