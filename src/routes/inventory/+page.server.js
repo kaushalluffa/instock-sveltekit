7232c4d68c887b3709950308205fe04cfef50243
kaushalluffa <kaushalluffa91@gmail.com>
2024-06-13 22:15:42 -0400

    feat(move-all-actions): move all inventory actions to one file for reusability

--- src/routes/inventory/+page.server.js
+++ src/routes/inventory/+page.server.js
@@ -1,4 +1,4 @@
-import { getInventories } from '$lib/server/appwrite.js';
+import { createInventory, deleteInventoryClient, getInventories, updateInventoryClient } from '$lib/server/appwrite.js';
 import { redirect } from '@sveltejs/kit';
 
 export const load = async ({ locals, cookies }) => {
@@ -7,4 +7,57 @@ export const load = async ({ locals, cookies }) => {
 	}
 	const inventories = await getInventories({ cookies });
 	return { inventories };
+};
+export const actions = {
+	addInventory: async ({ request, cookies }) => {
+		// Extract the form data.
+		const form = await request.formData();
+		const item_name = form.get('itemName');
+		const item_description = form.get('description');
+		const item_category = form.get('category');
+		const status = form.get('status');
+		const quantity = form.get('quantity');
+		const warehouse = form.get('warehouse');
+		const inventoryData = {
+			item_name,
+			item_description,
+			status,
+			quantity: Number(quantity),
+			warehouses: warehouse,
+			category: item_category,
+			createdAt: new Date().toISOString(),
+			updatedAt: new Date().toISOString()
+		};
+		const createdInventory = await createInventory({ cookies, inventoryData });
+		redirect(301, `/details/inventory/${createdInventory?.$id}`);
+	},
+	deleteInventory: async ({ cookies, request }) => {
+		const form = await request.formData();
+		const inventoryId = form.get('inventoryId');
+		await deleteInventoryClient({ cookies, inventoryId });
+	},
+	updateInventory: async ({ request, cookies }) => {
+		// Extract the form data.
+		const form = await request.formData();
+		const inventoryId = form.get('itemId');
+		const item_name = form.get('itemName');
+		const item_description = form.get('description');
+		const item_category = form.get('category');
+		const status = form.get('status');
+		const quantity = form.get('quantity');
+		const warehouse = form.get('warehouse');
+		const updatedInventoryData = {
+			$id: inventoryId,
+			item_name,
+			item_description,
+			status,
+			quantity: Number(quantity),
+			warehouses: warehouse,
+			category: item_category,
+			createdAt: new Date().toISOString(),
+			updatedAt: new Date().toISOString()
+		};
+		const createdInventory = await updateInventoryClient({ cookies, updatedInventoryData });
+		redirect(301, `/details/inventory/${createdInventory?.$id}`);
+	}
 };
